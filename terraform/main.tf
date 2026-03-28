4d65c1a7f380f5cad157b8d74e61d7aac62a1e82
prashant <pbeniwal@yahoo.com>
2026-03-28 12:25:30 +0530

    updated

--- terraform/main.tf
+++ terraform/main.tf
@@ -20,31 +20,31 @@ resource "google_project_service" "iam" {
 }
 
 # Firestore Database (Native mode)
-#resource "google_firestore_database" "default" {
-#  depends_on = [google_project_service.firestore]
+resource "google_firestore_database" "default" {
+  depends_on = [google_project_service.firestore]
 
-#  project     = var.project_id
-#  name        = "(default)"
-#  location_id = var.region
-#  type        = "FIRESTORE_NATIVE"
+  project     = var.project_id
+  name        = "(default)"
+  location_id = var.region
+  type        = "FIRESTORE_NATIVE"
 
-#  delete_protection_state = "DELETE_PROTECTION_DISABLED"  # For student project
-#}
+  delete_protection_state = "DELETE_PROTECTION_DISABLED"  # For student project
+}
 
 # Pub/Sub Topic
-#resource "google_pubsub_topic" "event_registrations" {
-#  depends_on = [google_project_service.pubsub]
-#  name       = "event-registrations"
-#}
+resource "google_pubsub_topic" "event_registrations" {
+  depends_on = [google_project_service.pubsub]
+  name       = "event-registrations"
+}
 
 # Pub/Sub Subscription (pull-based)
-#resource "google_pubsub_subscription" "event_registrations_sub" {
-#  depends_on = [google_pubsub_topic.event_registrations]
-#  name       = "event-registrations-sub"
-#  topic      = google_pubsub_topic.event_registrations.name
+resource "google_pubsub_subscription" "event_registrations_sub" {
+  depends_on = [google_pubsub_topic.event_registrations]
+  name       = "event-registrations-sub"
+  topic      = google_pubsub_topic.event_registrations.name
 
-#  ack_deadline_seconds = 20
-#}
+  ack_deadline_seconds = 20
+}
 
 # Service Account for Cloud Run (least privilege)
 #resource "google_service_account" "cloud_run_sa" {
@@ -53,23 +53,23 @@ resource "google_project_service" "iam" {
 #}
 
 # IAM Roles for Cloud Run SA
-resource "google_project_iam_member" "firestore_access" {
-  project = var.project_id
-  role    = "roles/datastore.user"
-  member  = "serviceAccount:${google_service_account.cloud_run_sa.email}"
-}
+#resource "google_project_iam_member" "firestore_access" {
+#  project = var.project_id
+#  role    = "roles/datastore.user"
+#  member  = "serviceAccount:${google_service_account.cloud_run_sa.email}"
+#}
 
-resource "google_project_iam_member" "pubsub_publisher" {
-  project = var.project_id
-  role    = "roles/pubsub.publisher"
-  member  = "serviceAccount:${google_service_account.cloud_run_sa.email}"
-}
+#resource "google_project_iam_member" "pubsub_publisher" {
+#  project = var.project_id
+#  role    = "roles/pubsub.publisher"
+#  member  = "serviceAccount:${google_service_account.cloud_run_sa.email}"
+#}
 
-resource "google_project_iam_member" "pubsub_subscriber" {
-  project = var.project_id
-  role    = "roles/pubsub.subscriber"
-  member  = "serviceAccount:${google_service_account.cloud_run_sa.email}"
-}
+#resource "google_project_iam_member" "pubsub_subscriber" {
+#  project = var.project_id
+#  role    = "roles/pubsub.subscriber"
+#  member  = "serviceAccount:${google_service_account.cloud_run_sa.email}"
+#}
 
 # Optional: Cloud Run Service (can be deployed via Terraform too)
 # (Comment out if you prefer Cloud Build deployment)
